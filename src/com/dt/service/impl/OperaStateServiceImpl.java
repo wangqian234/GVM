7fa7ad3164e6e02128efae82c10c661bb957159f
wangqian234 <565059807@qq.com>
2017-09-28 09:48:13 +0800

    zuixin

--- src/com/dt/service/impl/OperaStateServiceImpl.java
+++ src/com/dt/service/impl/OperaStateServiceImpl.java
@@ -1,6 +1,9 @@
 package com.dt.service.impl;
 
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,23 @@ public class OperaStateServiceImpl implements OperaStateService {
 	
 	public List<Object> getbaseInfo(String project, String facility) {
 		List<Object> list = operaStateDao.getbaseInfo(project, facility);
+		Object[] obj = null;
+		List<Map<String, String>> listmap = null;
+		Iterator<Object> it = list.iterator();
+		while (it.hasNext()) {
+			Map<String, String> map = new HashMap<String, String>();
+			obj = (Object[]) it.next();
+			map.put("Detector_EquipmentRoom_Name", obj[0].toString());
+			map.put("Detector_Equipment_Name", obj[1].toString());
+			map.put("Detector_Sensor_Name", obj[2].toString());
+			map.put("Detector_Sensor_AlarmValueMin", obj[3].toString());
+			map.put("Detector_Sensor_AlarmValueMax", obj[4].toString());
+			map.put("Detector_SensorData_Value", obj[5].toString());
+			map.put("Detector_SensorData_Type", obj[6].toString());
+			map.put("Detector_Sensor_Id", obj[7].toString());
+			listmap.add(map);
+		}
+		
 		return list;
 	}
 
